2cef5c1669c4b30ab2af6a4126a5ebf908874f2d
cedric <pailletcedric@gmail.com>
2016-05-03 01:14:18 +0200

    add -y to apt-get install

--- ressources/install.sh
+++ ressources/install.sh
@@ -5,7 +5,7 @@ echo "*             Installation des dépendances             *"
 echo "********************************************************"
 apt-get update
 echo 50 > /tmp/dependancy_nissan_leaf_connect_in_progress
-sudo apt-get install php5-mcrypt
+sudo apt-get -y install php5-mcrypt
 echo 100 > /tmp/dependancy_nissan_leaf_connect_in_progress
 echo "********************************************************"
 echo "*             Installation terminée                    *"
