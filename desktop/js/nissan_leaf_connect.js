2c2b401b3d5cd49b142ef9d7988b01ca62b106ec
Cedric Paillet <paillet_cedric@hotmail.com>
2017-09-30 07:58:53 +0200

    delete bootstrap switch

--- desktop/js/nissan_leaf_connect.js
+++ desktop/js/nissan_leaf_connect.js
@@ -33,8 +33,8 @@ function addCmdToTable(_cmd) {
     tr += '<input class="cmdAttr form-control input-sm" data-l1key="name" style="width : 140px;" placeholder="{{Nom}}">';
     tr += '</td>';
     tr += '<td>';
-    tr += '<span><input type="checkbox" class="cmdAttr bootstrapSwitch" data-size="mini" data-label-text="{{Historiser}}" data-l1key="isHistorized" /></span>';
-    tr += '<span><input type="checkbox" class="cmdAttr bootstrapSwitch" data-size="mini" data-l1key="isVisible" data-label-text="{{Afficher}}" checked/></span> ';
+    tr += '<span><input type="checkbox" class="cmdAttr" data-size="mini" data-label-text="{{Historiser}}" data-l1key="isHistorized" /></span>';
+    tr += '<span><input type="checkbox" class="cmdAttr" data-size="mini" data-l1key="isVisible" data-label-text="{{Afficher}}" checked/></span> ';
 
     tr += '</td>';
     tr += '<td>';
